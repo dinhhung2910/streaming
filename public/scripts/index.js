6f8b1244f4c00ac86141eb99e3751ae98e6b4ca5
dinhhung2910 <dinhhung2910@gmail.com>
2020-04-05 21:29:40 +0700

    + fix pause

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,19 +1,41 @@
 document.addEventListener('DOMContentLoaded', init, false);
 let videoPlayer;
 let socket = io.connect();
+let timestamp = {};
+
+function initTimestamp() {
+  timestamp.play = new Set();
+  timestamp.pause = new Set();
+  timestamp.seek = new Set();
+};
+initTimestamp();
+
+setInterval(() => {
+  // console.info('reseting time log...');
+  initTimestamp();
+}, 10000);
+
 
 socket.on('play', e => {
-  console.log('play', e);
-  videoPlayer.currentTime = e;
-  videoPlayer.play();
+  if (!timestamp.play.has(e)) {
+    console.log('play', e);
+    // not seek again
+    timestamp.seek.add(e);
+    videoPlayer.currentTime = e;
+    videoPlayer.play();
+  }
 })
 socket.on('pause', e => {
-  console.log('pause', e);
-  videoPlayer.pause();
+  if (!timestamp.pause.has(e)) {
+    console.log('pause', e);
+    videoPlayer.pause();
+  }
 })
 socket.on('seeked', e => {
-  console.log('seeked', e);
-  videoPlayer.currentTime = e;
+  if (!timestamp.seek.has(e)) {
+    console.log('seeked', e);
+    videoPlayer.currentTime = e;
+  }
 })
 
 /** 
@@ -32,22 +54,28 @@ function init() {
 
   videoPlayer.addEventListener("play", (e) => {
     console.log(e);
+    if (timestamp.play.has(e)) return;
     if (e.isTrusted) {
       socket.emit("play", videoPlayer.currentTime);
+      timestamp.play.add(e);
     }
   });
 
   videoPlayer.addEventListener("pause", (e) => {
     console.log(e);
+    if (timestamp.pause.has(e)) return;
     if (e.isTrusted) {
       socket.emit("pause", videoPlayer.currentTime);
+      timestamp.pause.add(e);
     }
   });
 
   videoPlayer.addEventListener("seeked", (e) => {
     console.log(e);
+    if (timestamp.seek.has(e)) return;
     if (e.isTrusted) {
       socket.emit("seeked", videoPlayer.currentTime);
+      timestamp.seek.add(e);
     }
   });
 }
