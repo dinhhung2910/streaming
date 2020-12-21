0c16f2127573b94409da98a5090c82ec206f785e
Hung Dinh <dinhhung2910@gmail.com>
2020-12-22 00:44:45 +0700

    + change player to plyr

--- client/components/moviePlayer.js
+++ client/components/moviePlayer.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import MovieSocket from './movieSocket';
+import Plyr from 'plyr-react';
+import useInterval from '../lib/useInterval';
 
 /**
  * @param {*} props props
@@ -20,6 +22,19 @@ function MoviePlayer(props) {
     }, 300);
   }, [props.movie]);
 
+  useInterval(() => {
+    const videoPlayer = document.querySelector('.video-container video');
+    if (videoPlayer && !videoPlayer.onplay && !videoPlayer.onpause) {
+      videoPlayer.id = movie.code;
+      videoPlayer.onplay = function(e) {
+        setPlayTimestamp(e.target.currentTime);
+      };
+      videoPlayer.onpause = function(e) {
+        setPauseTimestamp(e.target.currentTime);
+      };
+    }
+  }, 2000);
+
   return (
     <div id="video-container" className="video-container">
       <MovieSocket
@@ -28,34 +43,69 @@ function MoviePlayer(props) {
         playerId={playerId}
         movieId={movie ? movie.code : ''}/>
       {!movie ? null :
-        (<video
-          id={movie.code}
-          onPlay={(e) => {
-            setPlayTimestamp(e.target.currentTime);
-          }}
-          onPause={(e) => {
-            setPauseTimestamp(e.target.currentTime);
-          }}
-          controls>
-          {movie.sources.map((source, index) =>
-            (<source
-              key={index}
-              src={source.link}
-              type="video/mp4"
-              size={source.size} />))}
-          {movie.subtitles.map((item, index) => (
-            <track
-              key ={index}
-              kind='subtitles'
-              srcLang={item.srclang}
-              src={item.link}
-              label={item.language}
-            />
-          ))}
-        </video>) }
+        (
+          <div className="outer-1">
+            <div className="outer-2">
+              <PlyrPlayerMemo movie={movie} />
+            </div>
+          </div>
+        )
+        // (<video
+        //   id={movie.code}
+        //   onPlay={(e) => {
+        //     setPlayTimestamp(e.target.currentTime);
+        //   }}
+        //   onPause={(e) => {
+        //     setPauseTimestamp(e.target.currentTime);
+        //   }}
+        //   controls>
+        //   {movie.sources.map((source, index) =>
+        //     (<source
+        //       key={index}
+        //       src={source.link}
+        //       type="video/mp4"
+        //       size={source.size} />))}
+        //   {movie.subtitles.map((item, index) => (
+        //     <track
+        //       key ={index}
+        //       kind='subtitles'
+        //       srcLang={item.srclang}
+        //       src={item.link}
+        //       label={item.language}
+        //     />
+        //   ))}
+        // </video>)
+      }
     </div>
   );
 }
+
+const PlyrPlayer = ({movie}) => {
+  return (
+    <Plyr
+      source={{
+        type: 'video',
+        sources: (movie && movie.sources) ?
+          movie.sources.map((source) => ({
+            src: source.link,
+            type: 'video/mp4',
+            size: source.size,
+          })) : [],
+        tracks: (movie && movie.subtitles) ?
+          movie.subtitles.map((item) => ({
+            kind: 'subtitles',
+            srcLang: item.srclang,
+            src: item.link,
+            label: item.language,
+          })) : [],
+      }}
+    />
+  );
+};
+
+const PlyrPlayerMemo = React.memo(PlyrPlayer, (props, nextPtops) => {
+  return props.movie == nextPtops.movie;
+});
 
 MoviePlayer.propTypes = {
   'props': PropTypes.object,
--- client/lib/slices/notesSlice.js
+++ client/lib/slices/notesSlice.js
@@ -1,4 +1,7 @@
-import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit'
+import {
+  createAsyncThunk,
+  createSelector,
+  createSlice} from '@reduxjs/toolkit';
 
 export const addNote = createAsyncThunk(
   'notes/addNote',
@@ -10,66 +13,66 @@ export const addNote = createAsyncThunk(
         headers: {
           'Content-Type': 'application/json',
         },
-      })
+      });
 
       if (!response.ok) {
-        const error = await response.json()
+        const error = await response.json();
 
-        return thunkAPI.rejectWithValue({ error: error.errors })
+        return thunkAPI.rejectWithValue({error: error.errors});
       }
 
-      return response.json()
+      return response.json();
     } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.message })
+      return thunkAPI.rejectWithValue({error: error.message});
     }
-  }
-)
+  },
+);
 
 export const loadNotes = createAsyncThunk(
   'notes/loadNotes',
   async (_, thunkAPI) => {
     try {
-      const response = await fetch('/api/notes')
+      const response = await fetch('/api/notes');
 
-      return response.json()
+      return response.json();
     } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.message })
+      return thunkAPI.rejectWithValue({error: error.message});
     }
-  }
-)
+  },
+);
 
 export const editNote = createAsyncThunk(
   'notes/editNote',
   async (updates, thunkAPI) => {
-    const { id, title, content } = updates
+    const {id, title, content} = updates;
 
     try {
       const response = await fetch(`/api/notes?noteId=${id}`, {
         method: 'PUT',
-        body: JSON.stringify({ title, content }),
+        body: JSON.stringify({title, content}),
         headers: {
           'Content-Type': 'application/json',
         },
-      })
+      });
 
-      return response.json()
+      return response.json();
     } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.message })
+      return thunkAPI.rejectWithValue({error: error.message});
     }
-  }
-)
+  },
+);
 
 export const deleteNote = createAsyncThunk(
   'notes/deleteNote',
   async (id, thunkAPI) => {
     try {
-      await fetch(`/api/notes?noteId=${id}`, { method: 'DELETE' })
-      return id
+      await fetch(`/api/notes?noteId=${id}`, {method: 'DELETE'});
+      return id;
     } catch (error) {
-      return thunkAPI.rejectWithValue({ error: error.message })
+      return thunkAPI.rejectWithValue({error: error.message});
     }
-  }
-)
+  },
+);
 
 const notesSlice = createSlice({
   name: 'notes',
@@ -80,69 +83,69 @@ const notesSlice = createSlice({
   reducers: {},
   extraReducers: {
     [addNote.pending]: (state) => {
-      delete state.error
+      delete state.error;
     },
     [addNote.fulfilled]: (state, action) => {
-      state.notes.push(action.payload)
+      state.notes.push(action.payload);
     },
     [addNote.rejected]: (state, action) => {
-      state.error = action.payload.error
+      state.error = action.payload.error;
     },
     [loadNotes.pending]: (state) => {
-      state.notes = []
-      state.loading = 'loading'
+      state.notes = [];
+      state.loading = 'loading';
     },
     [loadNotes.fulfilled]: (state, action) => {
-      state.notes = action.payload
-      state.loading = 'loaded'
+      state.notes = action.payload;
+      state.loading = 'loaded';
     },
     [loadNotes.rejected]: (state, action) => {
-      state.loading = 'error'
-      state.error = action.payload.error
+      state.loading = 'error';
+      state.error = action.payload.error;
     },
     [editNote.pending]: (state, action) => {
-      const note = state.notes.find((note) => note.id === action.meta.arg.id)
-      state.tempNote = Object.assign({}, note)
-      note.title = action.meta.arg.title || note.title
-      note.content = action.meta.arg.content || note.content
+      const note = state.notes.find((note) => note.id === action.meta.arg.id);
+      state.tempNote = Object.assign({}, note);
+      note.title = action.meta.arg.title || note.title;
+      note.content = action.meta.arg.content || note.content;
     },
     [editNote.fulfilled]: (state, action) => {
-      const note = state.notes.find((note) => note.id === action.payload.id)
-      delete state.tempNote
-      Object.assign(note, action.payload)
+      const note = state.notes.find((note) => note.id === action.payload.id);
+      delete state.tempNote;
+      Object.assign(note, action.payload);
     },
     [editNote.rejected]: (state, action) => {
-      const note = state.notes.find((note) => note.id === action.meta.arg.id)
-      state.error = action.payload.error || action.error.message
-      Object.assign(note, state.tempNote)
-      delete state.tempNote
+      const note = state.notes.find((note) => note.id === action.meta.arg.id);
+      state.error = action.payload.error || action.error.message;
+      Object.assign(note, state.tempNote);
+      delete state.tempNote;
     },
     [deleteNote.pending]: (state, action) => {
       const position = state.notes.findIndex(
-        (note) => note.id === action.meta.arg
-      )
-      state.backupNote = Object.assign({}, state.notes[position])
-      state.backupPosition = position
-      state.notes.splice(position, 1)
+        (note) => note.id === action.meta.arg,
+      );
+      state.backupNote = Object.assign({}, state.notes[position]);
+      state.backupPosition = position;
+      state.notes.splice(position, 1);
     },
     [deleteNote.fulfilled]: (state) => {
-      delete state.backupNote
-      delete state.backupPosition
+      delete state.backupNote;
+      delete state.backupPosition;
     },
     [deleteNote.rejected]: (state) => {
-      state.notes.splice(state.backupPosition, 0, state.backupNote)
-      delete state.backupPosition
-      delete state.backupNote
+      state.notes.splice(state.backupPosition, 0, state.backupNote);
+      delete state.backupPosition;
+      delete state.backupNote;
     },
   },
-})
+});
 
 export const selectNotes = createSelector(
   (state) => ({
     notes: state.notes.notes,
     error: state.notes.error,
   }),
-  (state) => state
-)
+  (state) => state,
+);
 
-export default notesSlice.reducer
+export default notesSlice.reducer;
