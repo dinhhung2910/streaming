8fc97dc767e544913d5b65a8eaead8e37d384456
Hung Dinh <dinhhung2910@gmail.com>
2021-11-06 19:40:43 +0700

    * [client] Fix swiper compability with nextjs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,9 +2,8 @@ import {Provider} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import '../scss/style.scss';
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
+import '../public/css/swiper.min.css';
+
 import 'plyr/dist/plyr.css';
 
 import '../public/font-awesome/css/all.min.css';
