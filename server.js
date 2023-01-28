b4b598626298ce65ee3250c5fd167ecbf48bb280
Hung Dinh <dinhhung2910@gmail.com>
2023-01-28 14:55:04 +0700

    Config path from env

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const fs = require('fs')
 const path = require('path')
 var http = require('http');
 
+// Read config from .env file
+require('dotenv').config()
+
 const app = express();
 const connectDB = require('./config/db');
 
 var server  = http.createServer(app);
-
-// Read config from .env file
-require('dotenv').config()
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
